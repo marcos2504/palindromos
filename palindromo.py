02268b3b56539293860ef23d90761d16b2c0bc9f
marcos2504 <marcosibarra1234@gmail.com>
2022-10-05 13:52:05 -0300

    terminado

---
+++ palindromo.py
@@ -0,0 +1,11 @@
+
+
+def palindromo(palabra):
+    
+    if palabra.lower().replace(" ", "") == palabra[::-1].lower().replace(" ", ""):
+        return True
+    else:
+        return False
+   
+
+
---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+from palindromo import *
+class Testpalindromo(unittest.TestCase):
+    def test_1(self):
+        resultado = palindromo("ana")
+        self.assertEqual(resultado, True)
+
+    def test_2(self):
+        resultado = palindromo("Radar")
+        self.assertEqual(resultado, True)
+    
+    def test_3(self):
+        resultado = palindromo("Sale El As")
+        self.assertEqual(resultado, True)
+        
+
+if __name__ == '__main__':
+    unittest.main()
